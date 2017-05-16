52d67b54da9af16532f193a1620d5d6cf1839ff0
trentspi <tspice23@att.net>
2017-05-16 11:24:24 -0400

    removed unnecessary field variables and form declaration

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ const renderColor = (hairColor) => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
-
-  const form = event.target;
   const details = document.querySelector('.details');
-
-  const personName = form.personName.value;
-  const hairColor = form.hairColor.value;
-  const age = form.age.value;
-  const birthPlace = form.birthPlace.value;
-
-  const colorDiv = renderColor(hairColor);
 
   formValues(details);
 
